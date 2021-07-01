f3a1c95ec414ba329f23f042951c7066fc120a99
rg089 <rishabhforprojects@gmail.com>
2021-07-01 20:42:43 +0530

    corrected f-stirng

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ import os
 
 def connect():
   password = os.environ.get("password")
-  client = MongoClient("mongodb+srv://Vishal:{password}@newsemble.j40x6.mongodb.net/News?retryWrites=true&w=majority")
+  client = MongoClient(f"mongodb+srv://Vishal:{password}@newsemble.j40x6.mongodb.net/News?retryWrites=true&w=majority")
 
   #Db name and Collection Name
   db = client["News"]
